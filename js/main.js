e69fe446f6e9fd96a09d91f1e9832da47fdbb80b
savinaxu <jia_shijie@163.com>
2018-09-28 20:04:35 -0700

    finished homework

---
+++ js/main.js
@@ -0,0 +1,36 @@
+
+(function ($) {
+	"use strict";
+
+
+    //======= PRELOADER ========//
+    $(window).on('load',function () {
+        $('body').delay(550).css({
+            'overflow': 'visible'
+        });
+    });
+
+    //======= SITE NAVBAR ========//
+    var navMenu = $('.nav-menu')
+    	navMenu.onePageNav();
+    $(window).on('scroll resize',function() {
+    	var homeH = $('.home-section').height() - 90;
+    	if ($(this).scrollTop() > homeH) {
+    		$('nav').fadeIn(400);
+    		navMenu.onePageNav();
+    	}else{
+    		$('nav').fadeOut(200);
+    	}
+    	preventDefault();
+    });
+
+	
+    //======= RESPONSIVE MENU ========//
+    $('.mobile-bar').on('click', function() {
+    	$(this).toggleClass('active');
+    	$('.nav-menu').toggleClass('active');
+    	preventDefault();
+    });
+
+	
+})(jQuery);
