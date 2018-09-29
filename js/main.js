12b8cf90f1f3dcf27ca01b47d21c82a147d070ac
savinaxu <jia_shijie@163.com>
2018-09-28 20:20:54 -0700

    corrected some bugs.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
-
 (function ($) {
-	"use strict";
+    "use strict";
 
 
     //======= PRELOADER ========//
-    $(window).on('load',function () {
+    $(window).on('load', function () {
         $('body').delay(550).css({
             'overflow': 'visible'
         });
@@ -12,25 +11,25 @@
 
     //======= SITE NAVBAR ========//
     var navMenu = $('.nav-menu')
-    	navMenu.onePageNav();
-    $(window).on('scroll resize',function() {
-    	var homeH = $('.home-section').height() - 90;
-    	if ($(this).scrollTop() > homeH) {
-    		$('nav').fadeIn(400);
-    		navMenu.onePageNav();
-    	}else{
-    		$('nav').fadeOut(200);
-    	}
-    	preventDefault();
+    navMenu.onePageNav();
+    $(window).on('scroll resize', function () {
+        var homeH = $('.home-section').height() - 90;
+        if ($(this).scrollTop() > homeH) {
+            $('nav').fadeIn(400);
+            navMenu.onePageNav();
+        } else {
+            $('nav').fadeOut(200);
+        }
+        preventDefault();
     });
 
-	
+
     //======= RESPONSIVE MENU ========//
-    $('.mobile-bar').on('click', function() {
-    	$(this).toggleClass('active');
-    	$('.nav-menu').toggleClass('active');
-    	preventDefault();
+    $('.mobile-bar').on('click', function () {
+        $(this).toggleClass('active');
+        $('.nav-menu').toggleClass('active');
+        preventDefault();
     });
 
-	
+
 })(jQuery);
